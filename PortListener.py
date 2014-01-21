df5575d1568287fc4ecc6404dfea1f1c3c1d6222
doorux <scp8008@rit.edu>
2014-01-21 11:44:51 -0500

    Initial Commit

---
+++ PortListener.py
@@ -0,0 +1,10 @@
+'''
+	PortListener.py
+	Listens for requests on specific ports and spins up host-specific handlers (if configured)
+'''
+
+class PortListener:
+	def __init__(self):
+		return "Starting port-listener"
+		
+	
