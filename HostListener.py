4b9e1faf963eef5ea633ce5b3c12925db2fc1f1f
doorux <scp8008@rit.edu>
2014-01-23 14:00:23 -0500

    After fixing encoding issues to work uniformly for text and for blob files, updated logic to reflect the non-issue and stubbed out support for CGI files (which may be moved later, or this class renamed because it doesn't seem like the right place for it.)

--- HostListener.py
+++ HostListener.py
@@ -9,37 +9,22 @@ class HostListener:
 		self.config = configuration
 		print("New Hostlistener for: " + self.config.getConfig()['host'])
 		
-	def gettextURI(self, uri):
+	def getURI(self, uri):
 	
 		if uri == "/":
 			try:
-				with open('index.html', 'r') as f:
-					return {'status':200, 'text':f.read()}
+				with open('index.html', 'rb') as f:
+					return {'status':200, 'content':f.read()}
 			except FileNotFoundError as e:
 				print(e)
 				try:
-					with open('404.html', 'r') as f:
-						return {'status':404, 'text':f.read()}
+					with open('404.html', 'rb') as f:
+						return {'status':404, 'content':f.read()}
 				except FileNotFoundError as fnf:
 					print("SUPER BIG FILE NOT FOUND ERROR- COULDN'T EVEN FIND 404")
-					return {'status':404, 'text':"404! - 404 not found"}
+					return {'status':404, 'text':bytes("404! - 404 not found".encode())}
 							
 		elif uri != "":
-			try:
-				with open(uri.split("/")[1], 'r') as f:
-					return {'status':200, 'text':f.read()}
-			except FileNotFoundError as e:
-				print(e)
-
-				try:
-					with open('404.html', 'r') as f:
-						return {'status':404, 'text':f.read()}
-				except FileNotFoundError as fnf:
-					print("SUPER BIG FILE NOT FOUND ERROR- COULDN'T EVEN FIND 404")
-					return {'status':404, 'text':"404! - 404 not found"}
-	
-	def getmediaURI(self, uri):
-		if uri != "":
 			try:
 				with open(uri.split("/")[1], 'rb') as f:
 					return {'status':200, 'content':f.read()}
@@ -48,7 +33,12 @@ class HostListener:
 
 				try:
 					with open('404.html', 'rb') as f:
-						return {'status':404, 'text':f.read()}
+						return {'status':404, 'content':f.read()}
 				except FileNotFoundError as fnf:
 					print("SUPER BIG FILE NOT FOUND ERROR- COULDN'T EVEN FIND 404")
-					return {'status':404, 'text':"404! - 404 not found"}
+					return {'status':404, 'content':bytes("404! - 404 not found".encode())}
+	
+	def handleCGI(self, uri):
+		print("CGI Call")
+		return {'status':200, 'content':bytes("Not yet implemented. Bug the developer.".encode())}
+		
