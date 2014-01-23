0fbd8d7843e4cec3711eceff05f028335071a1d3
doorux <scp8008@rit.edu>
2014-01-23 13:57:29 -0500

    Updated for elegance and only to sort out cgi files. Everything else should either be a direct file/resource or blank, in which case, display the index.

--- GETRequest.py
+++ GETRequest.py
@@ -4,9 +4,7 @@
 import Request
 class GETRequest(Request.Request):
 
-	textExts = [ '.txt',  '.htm', '.html', '.doc', '.dat', '.xml', '.xhtml' ]
-	mediaExts = ['.jpg', '.jpeg', '.png', '.bmp', '.tif', '.tiff', '.ico', 
-				'.raw', '.mov', '.avi', '.mp4', '.mp3', '.mkv', '.rm', '.rmvb']
+	cgiExts = ['.php', '.py', '.aspx']
 
 	def __init__(self, req):
 		self.re = Request.Request(req)
@@ -18,10 +16,10 @@ class GETRequest(Request.Request):
 	def process(self, hostListener):
 		#request resource from HostListener
 		#if requested a type of text file
-		if( any(x in self.re.resource.lower() for x in self.textExts)
-			or self.re.resource == "/"):
-			return hostListener.gettextURI(self.re.resource)
+		if(self.re.resource == "/"):
+			return hostListener.getURI(self.re.resource)
 		#else if media type file is requested
-		elif(any(x in self.re.resource.lower() for x in self.mediaExts)):
-			return hostListener.getmediaURI(self.re.resource)
-		
+		elif(any(x in self.re.resource.lower() for x in self.cgiExts)):
+			return hostListener.handleCGI(self.re.resource)
+		else:
+			return hostListener.getURI(self.re.resource)
