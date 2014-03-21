3084944fd04cfe02a683c7ca3947983919d60f73
Spencer <doorux@hotmail.com>
2014-03-21 14:39:20 -0400

    Big changes! Cleaned up, then messed up code! Fixed sending so it actually sends full files and closes the connections when it needs to. Probably want to look into the .setblocking on the sockets, but for now it functions. Should clean up debugs in next push.

--- PortListener.py
+++ PortListener.py
@@ -7,6 +7,7 @@ import socket
 import time
 import RequestProcessor
 import signal
+import gzip
 
 class PortHandler(asyncore.dispatcher_with_send):
 			
@@ -28,23 +29,63 @@ class PortHandler(asyncore.dispatcher_with_send):
 		#h += "Content-type: text/html\n" #not including Content-type just yet. Needs refinement
 		h += 'Date: ' + current_date +'\n'
 		h += 'Server: Simple-Python-HTTP-Server\n'
+		#h += 'Content-Type: text/html; charset=UTF-8\n'
+		h += 'Content-Encoding: gzip\n'
 		h += 'Connection: close\n\n'  #tell browser we're closing the connection
 
 		return h
 		
-	def senddata(self, requestedData):	
+	def handle_write(self):	
+	
+		#process data
+		rp = RequestProcessor.RequestProcessor(self.request)
+		requestedData = rp.process(self.hostListener)
+			
 		body = ''
-		response_headers = self.gen_headers(requestedData['status'])
+		response_headers = self.gen_headers(requestedData['status']).encode()
 		
 		if 'text' in requestedData:
 			body = requestedData['text'].encode()
 		elif 'content' in requestedData:
 			body = requestedData['content']
-		
+				
 		#send data via the socket
-		self.send(response_headers.encode())
-		self.send(body)
+		try:
+			#lock down the socket so no one else can use it. might defeat 
+			#purpose of using asyncore, but asyncore was doing a bad job
+			#of handling things, causing sending to fail with "resource
+			#unavailable
+			self.socket.setblocking(True)
+			
+			sent = self.sendall(response_headers)
+			self.buffer = body
+			
+			#use gzip compression on body to shrink it a bit
+			self.buffer = gzip.compress(self.buffer)
+			
+			sent = len(self.buffer)
+			print("Body content size: ", end='')
+			print(sent)
+			
+			sent = self.sendall(self.buffer)
+			
+			self.handle_close()
+			#unblock socket I guess
+			self.socket.setblocking(False)
+						
+		except socket.error as err:
+			print("SOCKET ERROR: ", end="")
+			print(err)
+			self.handle_close()
+		
 	
+	def writeable(self):
+		return True
+	
+	def handle_close(self):
+		print("closing connection")
+		self.close()
+		
 	def handle_read(self):
 		msg = ''
 		chunk = 'blarg'
@@ -52,40 +93,36 @@ class PortHandler(asyncore.dispatcher_with_send):
 		chunk = self.recv(8192)
 		msg = bytes.decode(chunk)
 		
-		#process data
-		rp = RequestProcessor.RequestProcessor(msg)
-		requestedData = rp.process(self.hostListener)
-		self.senddata(requestedData)
+		self.request = msg
+		self.handle_write()
 
 
-class PortListener(asyncore.dispatcher):
+class PortListener(asyncore.dispatcher_with_send):
 
 	def __init__(self, listeningPort, hostListener):
-		asyncore.dispatcher.__init__(self)
 		self.hostListener = hostListener
 		self.bindAddr = hostListener.getBindAddress()
-		self.port = listeningPort; #default port to try listening on
+		self.port = listeningPort; #port to try listening on
+		
+		asyncore.dispatcher.__init__(self)
 		self.create_socket(socket.AF_INET, socket.SOCK_STREAM)
 		self.set_reuse_addr()
+		
 		#bind the socket to a public host and port
 		self.bind((self.bindAddr, self.port))
 		self.listen(5)
+		
 		#start the listeners
 		print( "Setting up port-listener on: ", end="") #debug
 		print( self.getsockname() )
 	
-	def graceful_shutdown(self, sig, dummy):
-		'''shutdown server from SIGINT signal'''
-		print("Shutting down server")
-		import sys
+		
+	def close_socket(self):
 		self.shutdown(0)
 		self.close()
-		sys.exit(1)
 		
 	def handle_accepted(self, sock, addr):
-			
-		signal.signal(signal.SIGINT, self.graceful_shutdown)
-					
+								
 		handler = PortHandler(sock)
 		handler.addHostListener(self.hostListener)
 		
